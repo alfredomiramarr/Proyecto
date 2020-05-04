f9821c142de7fec28e0879d146ba7d37f2062283
alfredomiramarr <alfredomiramarr@users.noreply.github.com>
2020-05-03 23:39:49 -0500

    Diseño de catálogos
    Nuevo diseño para mostrar las tablas y de los catálogos que tenemos en la base de datos.

--- cat_cultivos.php
+++ cat_cultivos.php
@@ -101,28 +101,125 @@
 	  <a href="cat_riego.php">RIEGO</a>
 	  <a href="cat_abonos.php">ABONO</a>
 	  <a href="cat_plagas.php">PLAGAS</a>
-	  <a href="cat_cosecha.php">COSECHA</a>
+	  <a href="cat_cosechas.php">COSECHA</a>
 	  <a href="cat_tratamientos.php">TRATAMIENTOS</a>
 
 	</div>
 		<div class="container-login100" style="background-image: url('images/banana-back.jpg');">
-			<div class="wrap-login100 p-l-50 p-r-50 p-t-30 p-b-30">
+			<style>
+				.tabla10 {
+				  width: 1000px;
+				  background: #fff;
+				  border-radius: 10px;
+				  overflow: hidden;
+				  align-items: center;
+
+				  box-shadow: 0 3px 20px 0px rgba(0, 0, 0, 0.1);
+				  -moz-box-shadow: 0 3px 20px 0px rgba(0, 0, 0, 0.1);
+				  -webkit-box-shadow: 0 3px 20px 0px rgba(0, 0, 0, 0.1);
+				  -o-box-shadow: 0 3px 20px 0px rgba(0, 0, 0, 0.1);
+				  -ms-box-shadow: 0 3px 20px 0px rgba(0, 0, 0, 0.1);
+				}
+			</style>
+			<div class="tabla10 p-l-50 p-r-50 p-t-30 p-b-30">
 				<form class="login100-form validate-form">
 					<div class="login100-form-logo" align="center">
 						<img src="images/logoempresa.png" alt="MILO" height="52" width="52">
 						<h1>CULTIVOS</h1>
 </head>
-			<table>
+				<style>
+					table.paleBlueRows {
+					  font-family: SourceSansPro-Regular;
+					  border: 3px solid white;
+					  width: 800px;
+					  height: 200px;
+					  text-align: center;
+					  border-collapse: collapse;
+					}
+					table.paleBlueRows td, table.paleBlueRows th {
+					  border: 5px solid white;
+					  padding: 5px 10px;
+					}
+					table.paleBlueRows tbody td {
+					  font-size: 17px;
+					}
+					table.paleBlueRows tr:nth-child(even) {
+					  background: #D0E4F5;
+					}
+					table.paleBlueRows thead {
+					  background: #3ADEE7;
+					  border-bottom: 5px solid white;
+					}
+					table.paleBlueRows thead th {
+					  font-family: SourceSansPro-Regular;
+					  font-size: 20px;
+					  font-weight: bold;
+					  color: #000000;
+					  text-align: center;
+					  border-left: 2px solid white;
+					}
+					table.paleBlueRows thead th:first-child {
+					  border-left: none;
+					}
+
+					table.paleBlueRows tfoot td {
+					  font-size: 14px;
+					}
+				</style>
+				<div class="paleBlueRows" align="center">
+				<table class="paleBlueRows" align="center">
+					<thead>
 					<tr>
-				
 					<th>NOMBRE DE CULTIVO</th>
 					<th>LOCACION</th>
 					<th>TIPO DE CULTIVO</th>
-					<th>CANTIDAD</th>
-					
-					
+					<th>CANTIDAD</th>		
 					</tr>
+					</thead>
+					<style>
+						.btn {
+						  display: -webkit-box;
+						  display: -webkit-flex;
+						  display: -moz-box;
+						  display: -ms-flexbox;
+						  display: flex;
+						  justify-content: center;
+						  align-items: center;
+						  padding: 0 10px;
+						  min-width: 70px;
+						  height: 15px;
+						  background-color: #3ADEE7;
+						  border-radius: 25px;
 
+						  font-family: SourceSansPro-SemiBold;
+						  font-size: 10px;
+						  color: black;
+						  line-height: 1.2;
+						  text-transform: uppercase;
+
+						  -webkit-transition: all 0.4s;
+						  -o-transition: all 0.4s;
+						  -moz-transition: all 0.4s;
+						  transition: all 0.4s;
+
+						  box-shadow: 0 10px 30px 0px rgba(189, 89, 212, 0.5);
+						  -moz-box-shadow: 0 10px 30px 0px rgba(189, 89, 212, 0.5);
+						  -webkit-box-shadow: 0 10px 30px 0px rgba(189, 89, 212, 0.5);
+						  -o-box-shadow: 0 10px 30px 0px rgba(189, 89, 212, 0.5);
+						  -ms-box-shadow: 0 10px 30px 0px rgba(189, 89, 212, 0.5);
+						}
+
+						.btn:hover {
+						  background-color: #4b2354;
+						  box-shadow: 0 10px 30px 0px rgba(189, 89, 212, 0.8);
+						  -moz-box-shadow: 0 10px 30px 0px rgba(189, 89, 212, 0.8);
+						  -webkit-box-shadow: 0 10px 30px 0px rgba(189, 89, 212, 0.8);
+						  -o-box-shadow: 0 10px 30px 0px rgba(189, 89, 212, 0.8);
+						  -ms-box-shadow: 0 10px 30px 0px rgba(189, 89, 212, 0.8);
+						}
+					</style>
+
+					<tbody>
 					<?php
 					//include("include/conexion.php");
 					$sql = "SELECT * FROM T_Crop;";
@@ -139,8 +236,8 @@
 					echo "<td> ". $row ["Crop_Type"]. "</td>";
 					echo "<td> ". $row ["Crop_Quant"]. "</td>";
 					
-					echo "<td> . <a href 'editar.php?no=".$row["Crop_ID"]."'> <button type='submit' class='ed'>Editar</button></a></td>";
-					echo "<td> . <a href 'modificar.php?no=".$row["Crop_ID"]."'> <button type='submit' class='el'>Eliminar</button></a></td>";
+					echo "<td> <a href 'editar.php?no=".$row["Crop_ID"]."'> <button type='submit'  name='editar' class='btn'>Editar</button></a><p>...</p>
+					<a href 'modificar.php?no=".$row["Crop_ID"]."'> <button type='submit' name='eliminar' class='btn'>Eliminar</button></a></div></td>";
 					
 					
 					echo "</tr>";
@@ -153,11 +250,13 @@
 					echo "Error";
 					}
 					?>
+					</tbody>
 				
-		</table>
+				</table>
+				</div>
 		<div class="text-center p-t-40 p-b-20">
-					<a href="crop.php"class="txt1">
-						New Crop
+					<a href="new_cultivo.php" class="txt1">
+						Nuevo Cultivo
 					</a>
 				</div>
 		</div>
